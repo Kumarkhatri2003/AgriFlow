9988b6864eee10c8a37eb85906870c9e1031f078
Kumar Khatri <kumarkhatri836@gmail.com>
2026-03-29 22:53:30 +0545

    fix : Simple fix for frontend integrations

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +52,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",   
+    "http://localhost:5174",   
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'config.urls'
